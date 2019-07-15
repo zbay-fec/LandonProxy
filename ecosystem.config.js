ed23d3194157aa35a0ae2b6c21fa4f7d030edbeb
Landon <landon.baker@utexas.edu>
2019-07-15 10:08:53 -0500

    pm2 setup

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'proxy',
+    script: 'server.js'
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'ec2-3-16-188-28.us-east-2.compute.amazonaws.com',
+      key: '~/.ssh/FECCarousel.pem',
+      ref: 'origin/master',
+      repo: 'https://github.com/zbay-fec/LandonProxy.git',
+      path: '/home/ubuntu/landonProxy',
+      'post-deploy': 'npm install && pm2 startOrRestart ./ecosystem.config.js'
+    }
+  }
+}
