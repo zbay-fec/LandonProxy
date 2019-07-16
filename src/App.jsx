c92c8fac727a4152807bc490d8336b3082442b55
Landon <landon.baker@utexas.edu>
2019-07-16 10:15:05 -0500

    add itemDetails component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 const App = () => (
   <>
   <Search />
   <Router>
-    <div className="app">
-      <Route exact path='/' component={Carousel} />
+    <div className='app'>
+      <Route exact path='/' components={{Carousel, ItemDetail}} />
       <Route path='/cart' component={Cart} />
     </div>
     
