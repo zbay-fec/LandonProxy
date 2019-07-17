da15bcd6d92adcd41bf2b5724cd4db16e017dc13
Landon <landon.baker@utexas.edu>
2019-07-17 15:03:28 -0500

    test conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,36 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showCart: false
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('showCart', e => this.setState({ showCart: e.detail.showCart }))
+  }
+
+  render() {
+    if (this.state.showCart) {
+      return (
+        <div>
+          <Search />
+          <Cart />  
+        </div>
+      ); 
+    } else {
+      return (
+        <div>
+          <Search />
+          <ItemDetail />
+          <Carousel />
+        </div>
+      );
+    }
+  }
+}
 
 const App = () => (
   <>
