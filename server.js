c279cfff8cccf3b5cb10a09cdd194fbbea050f80
Landon <landon.baker@utexas.edu>
2019-07-19 14:59:26 -0500

    remove get route

--- server.js
+++ server.js
@@ -4,12 +4,4 @@ const path = require('path');
 
 app.use(express.static(path.join(__dirname, '/public')));
 
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/index.html'), err => {
-    if (err) {
-      res.status(500).send(err);
-    }
-  });
-});
-
 app.listen(3000, () => console.log('proxy server up and running on port 3000'));
